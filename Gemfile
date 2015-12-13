86128bd8878a11fdd56236b608e80d1b6fbe135a
Rafael Huaman <rlevano@thoughtworks.com>
2015-12-13 14:44:18 -0500

    Add rspec/given. Add test for movie creation

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 gem 'simplecov' 
 gem 'rspec'
+gem 'rspec-given'
--- spec/video_store_spec.rb
+++ spec/video_store_spec.rb
@@ -1,9 +1,19 @@
 require 'spec_helper'
 require 'video_store'
+require 'rspec/given'
 
 describe Customer do
   it 'creates a customer' do
     expect(Customer.new('Mike')).to be_a(Customer)
   end
   
+end
+
+describe Movie do
+  Given(:valid_attributes) {{title: 'The Terminator', price_code: Movie::REGULAR}}
+  context 'when creating a new movie' do
+    When { @movie = Movie.new(valid_attributes[:title], valid_attributes[:price_code]) } 
+    Then { expect(@movie.title).to eq valid_attributes[:title] }
+    Then { expect(@movie.price_code).to eq valid_attributes[:price_code] }
+  end
 end
