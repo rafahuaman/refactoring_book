bb82e627bd1bc6976d90f36e5be1f7c3dfac6daf
Rafael Huaman <rlevano@thoughtworks.com>
2015-12-13 15:46:48 -0500

    Add test for rental

--- spec/lib/video_store_spec.rb
+++ spec/lib/video_store_spec.rb
@@ -12,8 +12,18 @@ end
 describe Movie do
   Given(:valid_attributes) {{title: 'The Terminator', price_code: Movie::REGULAR}}
   context 'when creating a new movie' do
-    When { @movie = Movie.new(valid_attributes[:title], valid_attributes[:price_code]) } 
-    Then { expect(@movie.title).to eq valid_attributes[:title] }
-    Then { expect(@movie.price_code).to eq valid_attributes[:price_code] }
+    When(:movie) { Movie.new(valid_attributes[:title], valid_attributes[:price_code]) } 
+    Then { expect(movie.title).to eq valid_attributes[:title] }
+    And  { expect(movie.price_code).to eq valid_attributes[:price_code] }
+  end
+end
+
+describe Rental do
+  Given(:movie) {double('movie')}
+  Given(:valid_attributes) {{movie: movie, days_rented: 1}}
+  context 'when creating a new Rental record' do
+    When(:rental) { Rental.new(valid_attributes[:movie], valid_attributes[:days_rented]) }
+    Then { expect(rental.movie).to be movie }
+    And  { expect(rental.days_rented).to eq valid_attributes[:days_rented] }
   end
 end
